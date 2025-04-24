877ef4af18a1995dafe9083f26d1a1297b08199f
4000m <16733682@qq.com>
2025-04-24 22:14:18 +0800

    完成该课程.

--- app/src/main/java/com/bluesky/emptywithcompose/LayoutActivity.kt
+++ app/src/main/java/com/bluesky/emptywithcompose/LayoutActivity.kt
@@ -1,6 +1,5 @@
 package com.bluesky.emptywithcompose
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +13,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,21 +28,32 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -50,19 +62,34 @@ import androidx.compose.ui.unit.dp
 import com.bluesky.emptywithcompose.ui.theme.EmptyWithComposeTheme
 
 class LayoutActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            /*todo 使用 calculateWindowSizeClass() 函数查看手机当前所用的配置。
+            * 窗口大小类别分为三种宽度：较小、中等和较大。
+            * 应用处于竖屏模式时，使用较小宽度；处于横屏模式时，使用较大宽度。
+            * 接受设备的 WindowSizeClass。如果宽度较小，则传入应用的纵向版本。
+            * 如果处于横向模式，则传入应用的横向版本。 */
+            val windowSizeClass = calculateWindowSizeClass(this)
             EmptyWithComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
-
-                        SearchBar()
-                    }
-
-                }
+                MySootheApp(windowSizeClass)
             }
+        }
+    }
+}
+
+@Composable
+fun MySootheApp(windowSize: WindowSizeClass) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MySootheAppPortrait()
+        }
+
+        WindowWidthSizeClass.Expanded -> {
+            MySootheAppLandscape()
         }
     }
 }
@@ -118,6 +145,7 @@ fun AlignYourBodyElement(
             modifier = Modifier
                 .size(88.dp)
                 .clip(CircleShape)
+
         )
         Text(
             text = stringResource(text),
@@ -191,3 +219,143 @@ fun FavioriteCollectionsGrid(modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int, modifier: Modifier = Modifier, content: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = (stringResource(title)),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    EmptyWithComposeTheme {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+    /*todo vertialScroll方法使得Column具备竖向滚动,也因此使其中的Image有足够的空间显示88dp的宽度,
+       进而使contentScale方法保持宽高一致,clip剪切能成为一个正圆,否则图片将被压扁*/
+    Column(modifier.verticalScroll(state = scrollState)) {
+        Spacer(Modifier.height(16.dp))
+
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow(modifier)
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavioriteCollectionsGrid(modifier)
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
+@Composable
+fun HomeScreenPreview() {
+    EmptyWithComposeTheme {
+        HomeScreen()
+    }
+}
+
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Default.Spa, contentDescription = null) },
+            label = { Text(stringResource(R.string.bottom_navigation_home)) },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null) },
+            label = { Text(stringResource(R.string.bottom_navigation_profile)) },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun MySootheAppPortrait() {
+    EmptyWithComposeTheme {
+        Scaffold(bottomBar = { SootheBottomNavigation() }) {
+            HomeScreen(Modifier.padding(it))
+        }
+
+
+    }
+}
+
+@Composable
+private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier
+            .padding(start = 8.dp, end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NavigationRailItem(
+                icon = { Icon(imageVector = Icons.Default.Spa, contentDescription = null) },
+                label = { Text(stringResource(R.string.bottom_navigation_home)) },
+                selected = true,
+                onClick = {}
+            )
+            Spacer(Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = { Text(stringResource(R.string.bottom_navigation_profile)) },
+                selected = false,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, widthDp = 960, heightDp = 432)
+@Composable
+fun MySootheAppLandscape() {
+    EmptyWithComposeTheme {
+        /*todo 在纵向版本中使用 Scaffold 时，它还会负责将内容颜色设置为背景。
+           如需设置导航栏的颜色，请将 Row 封装在 Surface 中，并将其设置为背景颜色。*/
+        Surface(color = MaterialTheme.colorScheme.background) {
+
+            Row {
+                SootheNavigationRail()
+                HomeScreen()
+            }
+        }
+    }
+}
+
